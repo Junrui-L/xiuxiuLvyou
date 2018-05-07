4c5f2d93f0b847988e20b76565bf037e2e3345af
lvjr <462293072@qq.com>
2018-05-07 19:22:42 +0800

    聊天页面，模板，css

---
+++ src/pages/webChat/Chat.vue
@@ -0,0 +1,209 @@
+<template>
+  <div class="Chat">
+    <div class="chat-header">
+      <div class="fl" @click="$router.go(-1)">
+        <svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" version="1.1">
+          <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#arrow-left"></use>
+          <!--<polyline points="12,18 4,9 12,0" style="fill:none;stroke:rgb(153,153,153);stroke-width:2"/>-->
+        </svg>
+        张三丰
+      </div>
+      <div class="fr">
+        <span class="chat-action">
+            ...
+         </span>
+      </div>
+    </div>
+    <div class="chat-content">
+      <div class="no-msg" >没有更多消息啦~</div>
+      <div class="x-message-group">
+        <div class="x-message-user">heigukeji</div>
+        <div class="x-message-content">
+          <p class="x-message-text">
+            哈哈哈哈
+          </p>
+        </div>
+        <div class="x-message-time">
+            05-06 11:35 AM
+        <span class="x-message-status"></span>
+      </div>
+      </div>
+      <div class="x-message-group x-message-right">
+        <div class="x-message-user"></div>
+        <div class="x-message-content">
+          <p class="x-message-text">
+            好啊
+          </p>
+        </div>
+        <div class="x-message-time">
+          05-06 11:35 AM
+          <span class="x-message-status"></span>
+        </div>
+      </div>
+      <div class="x-message-group">
+        <div class="x-message-user">heigukeji</div>
+        <div class="x-message-content">
+          <p class="x-message-text">
+            好开心啊，是不是啊
+          </p>
+        </div>
+        <div class="x-message-time">
+          05-06 11:35 AM
+          <span class="x-message-status"></span>
+        </div>
+      </div>
+      <div class="x-message-group x-message-right">
+        <div class="x-message-user"></div>
+        <div class="x-message-content">
+          <p class="x-message-text">
+            好啊
+          </p>
+        </div>
+        <div class="x-message-time">
+          05-06 11:35 AM
+          <span class="x-message-status"></span>
+        </div>
+      </div>
+      <div class="x-message-group">
+        <div class="x-message-user">heigukeji</div>
+        <div class="x-message-content">
+          <p class="x-message-text">
+            好开心啊，是不是啊
+          </p>
+        </div>
+        <div class="x-message-time">
+          05-06 11:35 AM
+          <span class="x-message-status"></span>
+        </div>
+      </div>
+      <div class="x-message-group x-message-right">
+        <div class="x-message-user"></div>
+        <div class="x-message-content">
+          <p class="x-message-text">
+            好啊
+          </p>
+        </div>
+        <div class="x-message-time">
+          05-06 11:35 AM
+          <span class="x-message-status"></span>
+        </div>
+      </div>
+      <div class="x-message-group">
+        <div class="x-message-user">heigukeji</div>
+        <div class="x-message-content">
+          <p class="x-message-text">
+            好开心啊，是不是啊
+          </p>
+        </div>
+        <div class="x-message-time">
+          05-06 11:35 AM
+          <span class="x-message-status"></span>
+        </div>
+      </div>
+      <div class="x-message-group x-message-right">
+        <div class="x-message-user"></div>
+        <div class="x-message-content">
+          <p class="x-message-text">
+            好啊
+          </p>
+        </div>
+        <div class="x-message-time">
+          05-06 11:35 AM
+          <span class="x-message-status"></span>
+        </div>
+      </div>
+      <div class="x-message-group">
+        <div class="x-message-user">heigukeji</div>
+        <div class="x-message-content">
+          <p class="x-message-text">
+            好开心啊，是不是啊
+          </p>
+        </div>
+        <div class="x-message-time">
+          05-06 11:35 AM
+          <span class="x-message-status"></span>
+        </div>
+      </div>
+      <div class="x-message-group x-message-right">
+        <div class="x-message-user"></div>
+        <div class="x-message-content">
+          <p class="x-message-text">
+            好啊
+          </p>
+        </div>
+        <div class="x-message-time">
+          05-06 11:35 AM
+          <span class="x-message-status"></span>
+        </div>
+      </div>
+      <div class="x-message-group">
+        <div class="x-message-user">heigukeji</div>
+        <div class="x-message-content">
+          <p class="x-message-text">
+            好开心啊，是不是啊
+          </p>
+        </div>
+        <div class="x-message-time">
+          05-06 11:35 AM
+          <span class="x-message-status"></span>
+        </div>
+      </div>
+      <div class="x-message-group x-message-right">
+        <div class="x-message-user"></div>
+        <div class="x-message-content">
+          <p class="x-message-text">
+            好啊
+          </p>
+        </div>
+        <div class="x-message-time">
+          05-06 11:35 AM
+          <span class="x-message-status"></span>
+        </div>
+      </div>
+    </div>
+    <div class="chat-footer">
+      <div class="chat-ops">
+        <div class="chat-ops-icon ib">
+          <div class="ib">
+            <a class="adropdown-trigger" href="#">
+              <i class="iconfont icon-biaoqing2" style="color: rgba(0, 0, 0, 0.65);"></i>
+            </a>
+          </div>
+        </div>
+        <label for="uploadImage" class="chat-ops-icon ib">
+          <i class="iconfont icon-tupian"></i>
+          <input type="file" id="uploadImage" class="hide">
+        </label>
+        <label for="uploadFile" class="chat-ops-icon ib">
+          <i class="iconfont icon-wenjian"></i>
+          <input type="file" id="uploadFile" class="hide">
+        </label>
+        <label for="clearMessage" class="chat-ops-icon ib">
+          <i class="iconfont icon-shanchu"></i>
+        </label>
+      </div>
+      <div class="chat-send">
+        <span class="input-group-wrapper">
+          <span class="input-wrapper">
+            <input type="text" placeholder="输入消息" class="input-txt fl">
+            <span class="input-group-addon">
+               <i class="iconfont icon-send" style="cursor: pointer;">
+            </i>
+            </span>
+          </span>
+        </span>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "chat-list"
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/webChat/ChatList.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="ChatList">
+    <div class="header">
+      <ul class="menu-horizontal clearfix"
+          id="x-header-tab" tabindex="0">
+        <li class="menu-item " :class="currentTab == 'chats' && 'item-selected'" @click="changeType('chats')">
+          聊天列表
+          <i class="iconfont icon-liaotianweixuanzhong" :class="currentTab == 'chats' && 'icon-liaotianxuanzhong1'"></i>
+        </li>
+        <li class="menu-item" :class="currentTab == 'records' && 'item-selected'" @click="changeType('records')">
+          群组
+          <i class="iconfont icon-05qunzu" :class="currentTab == 'records' && 'icon-05qunzufill'"></i>
+        </li>
+      </ul>
+    </div>
+    <div class="main-content">
+      <ul class="chats-list" v-if="currentTab == 'chats'">
+        <li class="chat-item" @click = "$router.push({path: '/chatList/33', query: {username: 1}})">
+          <div class="nav-text">
+            <div>wk19991024<span
+              class="ant-badge ant-badge-not-a-wrapper">33</span></div>
+            <div class="nav-text-desc">
+                    sss
+            </div>
+          </div>
+          <div class="nav-op">05-06 01:14 AM</div>
+        </li>
+        <li class="chat-item" @click = "$router.push({path: '/chatList/而且', query: {username: 1}})">
+          <div class="nav-text">
+            <div>张三丰<span
+              class="ant-badge ant-badge-not-a-wrapper">33</span></div>
+            <div class="nav-text-desc">
+              sss
+            </div>
+          </div>
+          <div class="nav-op">05-06 01:14 AM</div>
+        </li>
+        <li class="chat-item" @click = "$router.push({path: '/chatList/xdeaw', query: {username: 1}})">
+          <div class="nav-text">
+            <div>李小龙<span
+              class="ant-badge ant-badge-not-a-wrapper">33</span></div>
+            <div class="nav-text-desc">
+            </div>
+          </div>
+          <div class="nav-op">05-06 01:14 PM</div>
+        </li>
+
+      </ul>
+      <ul class="chats-list records-list" v-if="currentTab == 'records'">
+        <li class="chat-item">
+          <div class="nav-text">
+            wk19991024
+          </div>
+        </li>
+        <li class="chat-item">
+          <div class="nav-text">
+            张三丰
+          </div>
+        </li>
+        <li class="chat-item">
+          <div class="nav-text">
+            李小龙
+          </div>
+
+        </li>
+      </ul>
+    </div>
+    <transition name="slide-in">
+      <router-view></router-view>
+    </transition>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "chatlist",
+    data() {
+      return {
+        currentTab: 'chats'
+      }
+    },
+    mounted() {
+
+    },
+    methods: {
+      changeType(type) {
+        //返回消息列表
+        this.$router.push({path: '/chatList'})
+        this.currentTab = type;
+        if (type == 'chats') {
+          //TODO: 聊天列表
+        } else {
+          //联系人列表
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
